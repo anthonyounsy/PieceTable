47728206b769c6fb24cb9155f8a85e2f8b7df53f
anthonyounsy <123897512+anthonyounsy@users.noreply.github.com>
2023-03-09 18:05:29 -0600

    Update PieceTableTest.java

--- PieceTable/src/PieceTableTest.java
+++ PieceTable/src/PieceTableTest.java
@@ -6,29 +6,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
-import Archive.PieceTable;
 
-import java.io.*;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.io.*;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+
 public class PieceTableTest {
 	
-	 public static void main(String[] args) throws IOException {
+	 public static void main(String[] args) {
 
 	 }
 	
 	
     @Test
-	
     public void testInsert() {
     	PieceTable table= new PieceTable("Hello, world!");
     	table.insert(6, "everyone  ");
     	assertEquals("Hello,everyone   world!",table.getText());
     }
+    
+    @Test
+    public void testInsertInsert() {
+    	PieceTable table= new PieceTable("Hello, world!");
+    	table.insert(6, "everyone  ");
+    	assertEquals("Hello,everyone   world!",table.getText());
+    	table.insert(5, " ");
+    	assertEquals("Hello ,everyone   world!",table.getText());
+    }
+    
+    @Test
+    public void testManyInserts() {
+        PieceTable table = new PieceTable("");
+        StringBuilder test = new StringBuilder();
+        table.insert(0, " test");
+        test.insert(0, " test");
+        assertEquals(test.toString(), table.getText());
+        table.insert(3, " INSERT2INDEX0");
+        test.insert(3, " INSERT2INDEX0");
+        assertEquals(test.toString(), table.getText());
+        table.insert(3, " 1234567");
+        test.insert(3, " 1234567");
+        assertEquals(test.toString(), table.getText());
+        
+        table.insert(6, " hello world");
+        test.insert(6, " hello world");
+
+        assertEquals(test.toString(), table.getText());
+        
+    }
+    
     @Test
     public void testDelete() {
 //      PieceTable table = new PieceTable("Hello, world!");
@@ -76,11 +99,9 @@ public class PieceTableTest {
     public void testInsertAtBeginning() {
     	PieceTable table= new PieceTable("world!");
     	table.insert(0, "Hello, ");
-    	System.out.println(table.getSequence()); 
     	assertEquals("Hello, world!", table.getText());
 
     	}
-
 
 	@Test
 	public void testDeleteEntirePiece() {
@@ -88,16 +109,9 @@ public class PieceTableTest {
 	    table.delete(6,5);
 	    assertEquals("Hello,d!",table.getText());
 	}
+
 	
-	   @Test 
-	    public void testDeleteAndInsert() {
-	    	PieceTable table = new PieceTable("Hello, world!");
-	        table.delete(0,6);
-	        table.insert(0, "everyone");
-	        assertEquals("everyone world!", table.getText());
-	    }
-	
-	static String getAlphaNumericString(int n)
+	 static String getAlphaNumericString(int n)
 	 {
 	 
 	  // choose a Character random from this String
@@ -120,9 +134,10 @@ public class PieceTableTest {
 	   sb.append(AlphaNumericString
 	      .charAt(index));
 	  }
-	 
 	  return sb.toString();
 	 }
+	
+	
 	
 	public class PerformanceTest {
 		  private static final long MEGABYTE = 1024L * 1024L;
@@ -147,8 +162,6 @@ public class PieceTableTest {
 		    System.out.println("Used memory is bytes: " + memory);
 		    System.out.println("Used memory is megabytes: "
 		        + bytesToMegabytes(memory));
-		  }
-		} 
-    
-   
+		  }		} 
+	
 }
